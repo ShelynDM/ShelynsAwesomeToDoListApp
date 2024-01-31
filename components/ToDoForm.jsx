60f2fb4b56339c6556a13fa199dd11cc5113d117
Shelyn Del Mundo <dm.shelyn@gmail.com>
2024-01-31 09:13:41 -0700

    Lab 2 (Layout)

--- components/ToDoForm.jsx
+++ components/ToDoForm.jsx
@@ -1,4 +1,12 @@
 import React from 'react';
+import {
+  TextInput,
+  StyleSheet,
+  Button,
+  Pressable,
+  View,
+  Text,
+} from 'react-native';
 
 function ToDoForm(props) {
   return (
--- components/ToDoList.jsx
+++ components/ToDoList.jsx
@@ -1,4 +1,12 @@
 import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  Pressable,
+  View,
+  Text,
+} from 'react-native';
 
 function ToDoList(props) {
   return (
